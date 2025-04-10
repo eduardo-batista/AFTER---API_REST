f09e06b667ab1071eeea2f4d9ea9f7073e0a53bb
Eduardo Rocha Batista <eduardorochabat@gmail.com>
2025-04-09 23:53:01 -0300

    Chore: Melhorias na organização da infra

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$PRODUCTION" = "true" ]; then
-    exec python3 -m uvicorn main:app --workers 4 --host 0.0.0.0 --port 8000
+    exec python3 -m uvicorn main:app --workers $WORKERS --host 0.0.0.0 --port 8000
 else
     exec python3 -m debugpy --listen 0.0.0.0:5678 -m uvicorn main:app --reload --host 0.0.0.0 --port 8000
 fi

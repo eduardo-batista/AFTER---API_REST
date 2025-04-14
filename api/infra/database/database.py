293c41f3c1d9606d96b6ce4b067a2c6c9d979be0
Eduardo Rocha Batista <eduardorochabat@gmail.com>
2025-04-13 23:46:45 -0300

    Chore: Use Supabase

---
+++ api/infra/database/database.py
@@ -0,0 +1,78 @@
+"""
+database.py
+
+This module handles the database configuration and connection setup.
+"""
+import os
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker
+
+class DatabaseConfig:
+    """
+    Database configuration class responsible for loading environment variables
+    and creating an async SQLAlchemy engine.
+    """
+    _engine = None
+    def __init__(self):
+        """Initialize the database configuration by loading environment variables."""
+        load_dotenv()
+        self.config = {
+            "db_name": os.getenv("DB_NAME"),
+            "db_user": os.getenv("DB_USER"),
+            "db_password": os.getenv("DB_PASSWORD"),
+            "db_host": os.getenv("DB_HOST"),
+            "db_port": os.getenv("DB_PORT"),
+            "db_type": os.getenv("DB_TYPE"),
+            "db_async_driver": os.getenv("DB_ASYNC"),
+        }
+
+    def get_database_url(self) -> str:
+        """Construct and return the database URL."""
+        db_url = (
+            f"{self.config['db_type']}+{self.config['db_async_driver']}://"
+            f"{self.config['db_user']}:{self.config['db_password']}@"
+            f"{self.config['db_host']}"
+            f"{':' + self.config['db_port'] if self.config['db_port'] else ''}/"
+            f"{self.config['db_name']}"
+        )
+        return db_url
+
+    def get_async_engine(self) -> AsyncEngine:
+        """Create and return an async SQLAlchemy engine."""
+        if not self._engine:
+            self._engine = create_async_engine(
+                self.get_database_url(),
+                echo=False,
+                pool_pre_ping=True,
+                pool_size=10,
+                max_overflow=10,
+                pool_timeout=30
+            )
+        return self._engine
+
+    def get_async_session_local(self) -> async_sessionmaker:
+        """Create and return an async session factory."""
+        engine = self.get_async_engine()
+        return async_sessionmaker(bind=engine, autoflush=True, expire_on_commit=False)
+    
+    async def get_session(self):
+        """
+        Provides an asynchronous context manager for a database session.
+
+        This method creates and yields a session object, ensuring that the session 
+        is properly closed after usage, even if an exception occurs.
+
+        Yields:
+            AsyncSession: The session object used for database operations.
+
+        Example:
+            async with get_session() as session:
+                # Perform database operations
+        """
+        session_factory = self.get_async_session_local()
+        session = session_factory()
+        try:
+            yield session
+        finally:
+            await session.close()
